1c785a1ac2e8ebf92d41120608f532aa7a056b7c
djilanov <djilanov@gmail.com>
2019-06-29 15:34:55 +0200

    Add working structure of the main container

--- frontend/src/containers/FormContainer.js
+++ frontend/src/containers/FormContainer.js
@@ -4,6 +4,15 @@ import { getNgrams } from '../actions'
 import NgramsForm from '../components/NgramsForm'
 import NgramsTable from '../components/NgramsTable'
 
+const FormContainer = () => {
+  return (
+    <div>
+      <NgramsForm />
+      <NgramsTable />
+    </div>
+  )
+}
+
 const mapStateToProps = state => {
   return {
     ngrams: state.ngrams
@@ -17,4 +26,4 @@ const mapDispatchToProps = (dispatch) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(() => <div>{NgramsForm}{NgramsTable}</div>)
+)(FormContainer)
