cb618e5b840a864d798ac9556d1728088a126965
djilanov <djilanov@gmail.com>
2019-06-30 23:18:08 +0300

    Add basic response check

--- backend/routes/ngrams/ngrams-controller.js
+++ backend/routes/ngrams/ngrams-controller.js
@@ -1,21 +1,20 @@
 const ngramsController = (repository) => {
   const getNgrams = async (req, res) => {
-    // if (params.ok !== 0) {
+    if (req.query.text) {
       res.status(200).send({
         "He": 1,
         "el": 1,
         "ll": 1,
         "lo": 1
       });
-    // } else {
-    //   res.status(400).send(
-    //     {
-    //       error: response,
-    //       isSuccessful: false,
-    //       message: 'Provide a correct portfolio ID!'
-    //     }
-    //   );
-    // }
+    } else {
+      res.status(400).send(
+        {
+          error: true,
+          message: 'Provide a correct text!'
+        }
+      );
+    }
   };
 
   return {
